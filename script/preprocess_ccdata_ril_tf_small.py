66d009a55f99afa6b52a8083543bb883ec2ad638
Tran Ngoc Minh <minhtn@trans-mbp.mul.ie.ibm.com>
2017-06-08 14:46:40 +0100

    fix bug preprocess_youdata_ril_tf

--- script/preprocess_ccdata_ril_tf_small.py
+++ script/preprocess_ccdata_ril_tf_small.py
@@ -16,8 +16,9 @@ from sentana.utils.tf_utils import bytes_feature
 from sentana.utils.tf_utils import int64_feature
 
 
-IM_DIR = "/dccstor/sentana/sentana/you_data/images"
-OUT_DIR = "/dccstor/sentana/sentana/model/you_data"
+IM_DIR = "/dccstor/sentana/sentana/cc_data"
+OUT_DIR = "/dccstor/sentana/sentana/model/cc_data"
+LABEL = "/dccstor/sentana/sentana/Dataset_Vso/ANPs/3244ANPs.txt"
 IM_SIZE = 256
 
 
@@ -27,16 +28,33 @@ def process():
     :return:
     """
     # Create label indices
+    _, label_dict = get_labels(LABEL)
+
+    # Process images for each label, do in parallel
+    #labels = list(label_dict.keys())
     labels = os.listdir(IM_DIR)
-    labels = [l for l in labels if not l.startswith(".")]
+    labels = [l for l in labels if not l.startswith(".")
+              and l in label_dict.keys()]
+    jobs = []
+    lpp = int(len(labels) / 4)
+    for i in range(4):
+        if i < 3:
+            p = multiprocessing.Process(
+                target=worker, args=(labels[i*lpp:(i+1)*lpp], label_dict))
+        else:
+            p = multiprocessing.Process(target=worker,
+                                        args=(labels[i*lpp:], label_dict))
 
-    # Process images for each label
-    worker(labels)
+        jobs.append(p)
+        p.start()
+        p.join()
+        print(p.pid)
 
-    # Merge images for ril data
-    merge(labels, "train")
-    merge(labels, "valid")
-    merge(labels, "test")
+    # Start merging
+    print("Start merging...")
+    multiprocessing.Process(target=merge, args=(labels, "train")).start()
+    multiprocessing.Process(target=merge, args=(labels, "valid")).start()
+    multiprocessing.Process(target=merge, args=(labels, "test")).start()
 
 
 def merge(labels, part):
@@ -62,25 +80,47 @@ def merge(labels, part):
                 del readers[i]
                 break
 
-            if np.random.rand() < 0.3: break
+            if np.random.rand() < 0.01: break
 
     writer.close()
 
 
-def worker(labels):
+def get_labels(filename):
     """
-    Process each label.
+    Parse the visual sentiment ontology file to get sentiment values.
+    :param filename:
+    :return:
+    """
+    with open(filename) as f:
+        content = f.readlines()[3:]
+
+    label_dict = {}
+    anp_dict = {}
+    for line in content:
+        if line.startswith("\t"):
+            label_dict[line.split(" ")[0][1:]] = float(line.split(" ")[2][:-1])
+            anp_dict[line.split(" ")[0][1:]] = float(line.split(" ")[2][:-1])
+        else:
+            label_dict[line.split(" ")[1]] = float(line.split(" ")[3][:-1])
+
+    return label_dict, anp_dict
+
+
+def worker(labels, label_dict):
+    """
+    This function is run in parallel.
     :param labels:
+    :param label_dict:
     :return:
     """
     for label in labels:
-        # Writers for tfrecords
-        tf_writer_train = tf.python_io.TFRecordWriter(os.path.join(
-            OUT_DIR, "tf_train", label + ".tftrain"))
-        tf_writer_valid = tf.python_io.TFRecordWriter(os.path.join(
-            OUT_DIR, "tf_valid", label + ".tfvalid"))
-        tf_writer_test = tf.python_io.TFRecordWriter(os.path.join(
-            OUT_DIR, "tf_test", label + ".tftest"))
+        # # Writers for tfrecords
+        # tf_writer_train = tf.python_io.TFRecordWriter(os.path.join(
+        #     OUT_DIR, "tf_train", label + ".tftrain"))
+        # tf_writer_valid = tf.python_io.TFRecordWriter(os.path.join(
+        #     OUT_DIR, "tf_valid", label + ".tfvalid"))
+        # tf_writer_test = tf.python_io.TFRecordWriter(os.path.join(
+        #     OUT_DIR, "tf_test", label + ".tftest"))
 
         # Writer for ril
         ril_writer_train = bz2.BZ2File(os.path.join(OUT_DIR, "train",
@@ -94,19 +134,18 @@ def worker(labels):
         files = os.listdir(os.path.join(IM_DIR, label))
         files = [os.path.join(IM_DIR, label, f) for f in files
                  if not f.startswith(".")]
-        label_score = int(label.split(sep="_")[1])
+        label_score = 1 if label_dict[label] >= 0 else 0
 
         worker_assist(files[:int(0.8*len(files))], label_score,
-                      tf_writer=tf_writer_train, ril_writer=ril_writer_train)
+                      tf_writer=None, ril_writer=ril_writer_train)
         worker_assist(files[int(0.8*len(files)):int(0.9*len(files))],
-                      label_score, tf_writer=tf_writer_valid,
-                      ril_writer=ril_writer_valid)
+                      label_score, tf_writer=None, ril_writer=ril_writer_valid)
         worker_assist(files[int(0.9*len(files)):], label_score,
-                      tf_writer=tf_writer_test, ril_writer=ril_writer_test)
+                      tf_writer=None, ril_writer=ril_writer_test)
 
-        tf_writer_train.close()
-        tf_writer_valid.close()
-        tf_writer_test.close()
+        # tf_writer_train.close()
+        # tf_writer_valid.close()
+        # tf_writer_test.close()
         ril_writer_train.close()
         ril_writer_valid.close()
         ril_writer_test.close()
@@ -131,8 +170,8 @@ def worker_assist(files, label_score, tf_writer, ril_writer):
         sd_image = process_image(file)
 
         # Write tf records
-        tf_record = wrap_image(sd_image, label_score)
-        tf_writer.write(tf_record.SerializeToString())
+        #tf_record = wrap_image(sd_image, label_score)
+        #tf_writer.write(tf_record.SerializeToString())
 
         # Write ril records
         line = {"img": sd_image, "label": label_score}
